00606c28642d428fe8238f9e49adaa2afd4857d5
cladams0203 <cladams0203@gmail.com>
2020-06-24 18:50:40 -0400

    merging

--- src/components/createIssue.js
+++ src/components/createIssue.js
@@ -1,71 +1,72 @@
-import React from 'react';
-import { useForm } from 'react-hook-form';
-import { api } from '../utils/api';
+import React from "react";
+import { useForm } from "react-hook-form";
+import { api } from "../utils/api";
 
 function CreateIssue(props) {
-    const { register, handleSubmit, errors } = useForm();
-    const postIssue = data => {
-        // console.log('CreateIssue data', data);
-        api()
-        .post('/issues', data)
-        .then((res) => {
-            // console.log('post request response', res);
-        })
-        .catch((error) => { console.log('CreateIssue error',error)})
-    };
+  const { register, handleSubmit, errors } = useForm();
+  const postIssue = (data) => {
+    // console.log('CreateIssue data', data);
+    api()
+      .post("/issues", data)
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((error) => {
+        console.log("CreateIssue error", error);
+      });
+  };
 
-    return(
-        <div>
-            <form onSubmit={handleSubmit(postIssue)}>
-                <label>Title:</label>
-                <input
-                    type='text'
-                    name='title'
-                    ref={register({
-                        required: true
-                    })}
-                />
-                {errors.title && <span>Please enter a title.</span>}
+  return (
+    <div>
+      <form onSubmit={handleSubmit(postIssue)}>
+        <label>Title:</label>
+        <input
+          type="text"
+          name="title"
+          ref={register({
+            required: true,
+          })}
+        />
+        {errors.title && <span>Please enter a title.</span>}
 
-                <label>City:</label>
-                <input
-                    type='text'
-                    name='city'
-                    ref={register({
-                        required: true,
-                        pattern: /^[A-Za-z ]+$/
-                    })}
-                />
-                {errors.city && <span>Please enter a city using only letters and spaces.</span>}
+        <label>City:</label>
+        <input
+          type="text"
+          name="city"
+          ref={register({
+            required: true,
+            pattern: /^[A-Za-z ]+$/,
+          })}
+        />
+        {errors.city && (
+          <span>Please enter a city using only letters and spaces.</span>
+        )}
 
-                <label>Home Owners Association:</label>
-                <input
-                    type='text'
-                    ref={register}
-                />
-                <label>Description:</label>
-                <input 
-                type='text'
-                name='description'
-                ref={register({
-                    required: true,
-                })}
-                />
-                {errors.description && <span>Please enter a description.</span>}
+        <label>Home Owners Association:</label>
+        <input type="text" ref={register} />
+        <label>Description:</label>
+        <input
+          type="text"
+          name="description"
+          ref={register({
+            required: true,
+          })}
+        />
+        {errors.description && <span>Please enter a description.</span>}
 
-                <label>Add Photo:</label>
-                <input 
-                type='url'
-                name='photo'
-                ref={register({
-                    // pattern: /^[a-zA-Z0-9\-\.]+\.(com|org|net|mil|edu|COM|ORG|NET|MIL|EDU)$/
-                })}
-                />
-                {errors.photo && <span>Please enter a valid image URL.</span>}
-                <input type='submit'/>
-            </form>
-        </div>
-    );
-};
+        <label>Add Photo:</label>
+        <input
+          type="url"
+          name="image"
+          ref={register({
+            // pattern: /^[a-zA-Z0-9\-\.]+\.(com|org|net|mil|edu|COM|ORG|NET|MIL|EDU)$/
+          })}
+        />
+        {errors.photo && <span>Please enter a valid image URL.</span>}
+        <input type="submit" />
+      </form>
+    </div>
+  );
+}
 
 export default CreateIssue;
--- src/components/userDash.js
+++ src/components/userDash.js
@@ -1,53 +1,54 @@
-import React, { useEffect, useState } from 'react';
-import Issue from './issue';
+import React, { useEffect, useState } from "react";
+import Issue from "./issue";
 import { api } from "../utils/api";
-import { useForm } from 'react-hook-form';
+import { useForm } from "react-hook-form";
 import { useRecoilValue } from "recoil";
 import { userState } from "../recoil/atoms";
 
-
 function UserDash(props) {
-    const { editButtonState, issues, getIssues } = props;
-    const { register, handleSubmit } = useForm();
-    const [ issueFilter, setIssueFilter ] = useState('issues');
-    const [ userId, setUserId ] = useState(`0`);
-    const user = useRecoilValue(userState).user;
-    console.log(user);
-    
-    const onSearch = (data) => {
-        setIssueFilter(`issues/search?city=${data.search}`)
-    };
+  const { editButtonState, issues, getIssues } = props;
+  const { register, handleSubmit } = useForm();
+  const [issueFilter, setIssueFilter] = useState("issues");
+  const [userId, setUserId] = useState(`0`);
+  const user = useRecoilValue(userState).user;
+  console.log(user);
 
-    
-    useEffect(() => {
-        api()
-        .get(`https://lambda-co-make.herokuapp.com/${issueFilter}`)
-        .then((res) => {
-        //   console.log('Issues from the backend',res.data);
-          getIssues(res.data.data)
-        })
-        .catch((err) => console.log(err));
-    },[issueFilter]);
-    return(
-        <div>
-            <div>
-                <button onClick={() => setIssueFilter('issues')}>All Issues</button>
-                <button onClick={() => setIssueFilter(`api/users/${userId}:/issues`)}>Your Issues</button>
-                <form onSubmit={handleSubmit(onSearch)}>
-                    <label>Search for issues by location:</label>
-                    <input
-                        type='text'
-                        name='search'
-                        ref={register}
-                    />
-                    <input type='submit'/>
-                </form>
-            </div>
-            {issues && issues.map(issue => (
-            <Issue key={issue.id} issue={issue} editButtonState={editButtonState}/>
-            ))}
-        </div>
-    );
-};
+  const onSearch = (data) => {
+    setIssueFilter(`issues/search?city=${data.search}`);
+  };
 
-export default UserDash
+  useEffect(() => {
+    api()
+      .get(`/${issueFilter}`)
+      .then((res) => {
+        console.log(res);
+        getIssues(res.data.data);
+      })
+      .catch((err) => console.log(err));
+  }, [issueFilter]);
+  return (
+    <div>
+      <div>
+        <button onClick={() => setIssueFilter("issues")}>All Issues</button>
+        <button onClick={() => setIssueFilter(`api/users/${userId}:/issues`)}>
+          Your Issues
+        </button>
+        <form onSubmit={handleSubmit(onSearch)}>
+          <label>Search for issues by location:</label>
+          <input type="text" name="search" ref={register} />
+          <input type="submit" />
+        </form>
+      </div>
+      {issues &&
+        issues.map((issue) => (
+          <Issue
+            key={issue.id}
+            issue={issue}
+            editButtonState={editButtonState}
+          />
+        ))}
+    </div>
+  );
+}
+
+export default UserDash;
