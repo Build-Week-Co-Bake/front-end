2fc259ec135d49e3af791f253009d3dbfe795cfa
Nathan DeSpain <nathandespain@Nathans-MacBook-Pro.local>
2020-06-24 10:27:36 -0700

    UserDash styled

--- src/components/userDash.js
+++ src/components/userDash.js
@@ -16,11 +16,48 @@ function UserDash(props) {
     const user = useRecoilValue(userState).user;
     console.log(user);
     
+    const Cards = styled.div`
+        div.filters{
+            display: flex;
+            max-width: 1200px;
+            margin: 0 auto;
+            button{
+                padding: 1%;
+                font-size: 1rem;
+            }
+            form{
+                display: flex;
+                flex-wrap: wrap;
+                padding: 1%;
+                width: 100%;
+                label{
+                    font-size: 1rem;
+                    width: 100%;
+                    padding: 1%;
+                }
+                input{
+                    width: 60%;
+                    padding: 1%;
+                    margin: 1%;
+                }
+                input.submit{
+                    font-size: 1rem;
+                    padding: 1%;
+                    width: auto;
+                }
+            }
+        }
+        div.issues{
+            max-width: 1200px;
+            margin: 0 auto;
+        }
+        
+    `
+
     const onSearch = (data) => {
         setIssueFilter(`issues/search?city=${data.search}`)
     };
 
-    
     useEffect(() => {
         api()
         .get(`https://lambda-co-make.herokuapp.com/${issueFilter}`)
@@ -31,8 +68,8 @@ function UserDash(props) {
         .catch((err) => console.log(err));
     },[issueFilter]);
     return(
-        <div>
-            <div>
+        <Cards>
+            <div className='filters'>
                 <button onClick={() => setIssueFilter('issues')}>All Issues</button>
                 <button onClick={() => setIssueFilter(`api/users/${userId}:/issues`)}>Your Issues</button>
                 <form onSubmit={handleSubmit(onSearch)}>
@@ -42,13 +79,15 @@ function UserDash(props) {
                         name='search'
                         ref={register}
                     />
-                    <input type='submit'/>
+                    <input className='submit' type='submit'/>
                 </form>
             </div>
-            {issues && issues.map(issue => (
-            <Issue key={issue.id} issue={issue} editButtonState={editButtonState}/>
-            ))}
-        </div>
+            <div className='issues'>
+                {issues && issues.map(issue => (
+                <Issue key={issue.id} issue={issue} editButtonState={editButtonState}/>
+                ))}
+            </div>
+        </Cards>
     );
 };
 
